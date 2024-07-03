f1fcc989d8977991289e14f9c22ce796d17301e5
Blackbeing <jonnieey55@gmail.com>
2024-07-03 19:08:53 +0300

    feat List of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const students = [
+    { id: 1, name: 'Guillaume', location: 'San Francisco' },
+    { id: 2, name: 'James', location: 'Columbia' },
+    { id: 5, name: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
