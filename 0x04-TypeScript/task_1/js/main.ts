7b46e0cd17199074d68e909c2e96cfe3786834a0
Blackbeing <jonnieey55@gmail.com>
2024-07-04 00:00:00 +0300

    feat Constructor class interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,37 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
   return `${firstName[0]}. ${lastName}`;
 };
 
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInstance;
+}
+
+interface StudentInstance {
+  firstName: string;
+  lastName: string;
+  workOnHomeWork(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInstance {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomeWork(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+function createStudent(ctor: StudentConstructor, firstName: string, lastName: string): StudentInstance {
+  return new ctor(firstName, lastName)
+}
+
