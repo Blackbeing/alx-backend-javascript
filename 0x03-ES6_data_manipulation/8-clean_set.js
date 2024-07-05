327dde8fa67e2896f615ffb6c186790a5eca21ae
Blackbeing <jonnieey55@gmail.com>
2024-07-05 18:32:45 +0300

    feat Filter a set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const filset = [];
+  set.forEach((element) => {
+    if (element.startsWith(startString)) {
+      filset.push(element.slice(startString.length));
+    }
+  });
+  return filset.join('-');
+}
